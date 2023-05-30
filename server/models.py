cb08749da1983849388a6e8216547d6d1df07cfb
Yinson Tso <yinson.tso@gmail.com>
2023-05-30 14:56:21 -0400

    done?

--- server/models.py
+++ server/models.py
@@ -11,7 +11,20 @@ class Author(db.Model):
     phone_number = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-
+    
+    @validates('name')
+    def validate_name(self, key, name):
+        names = db.session.query(Author.name).all()
+        if not name:
+            raise ValueError("Requires a name")
+        elif name in names:
+            raise ValueError("Must be unique")
+    
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if len(phone_number) != 10:
+            raise ValueError("10 Digit Phone Number")
+    
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -27,6 +40,21 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('title')
+    def validate_title(self, key, title):
+        if not title:
+            raise ValueError("Requires a Title")
+        
+    @validates('content', 'summary')
+    def validate_text_length(self, key, text):
+        if key == 'content' and len(text) < 250:
+            raise ValueError("Text must be above 250 Characters")
+        elif key == 'summary' and len(text) > 250:
+            raise ValueError("Text must be below 250 Characters")
 
+    @validates('category')
+    def validate_category(self, key, category):
+        if category != 'Fiction' or category != 'Non-Fiction':
+            raise ValueError("Must be either Fiction or Non-Fiction")
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
